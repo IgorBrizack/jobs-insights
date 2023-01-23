82c598942db22198237b28f0f2d1554009dc1da4
Igor Brizack <igor_brizack@hotmail.com>
2023-01-23 17:28:51 -0300

    requisito 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,16 @@
+from src.insights.jobs import read
 from typing import List, Dict
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    industries_list = []
+    result_from_read = read(path)
+    for industries in result_from_read:
+        if industries['industry'] not in industries_list and :
+            if len(industries['industry']):
+                print(industries['industry'])
+                industries_list.append(industries['industry'])
+    return industries_list
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
